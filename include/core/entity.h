7a9b713b47e3558ce7244edc82b312004566eaf3
Richard <richcrwo@gmail.com>
2026-01-18 20:01:27 +0300

    change entity
    add entity init with default value and auto init collision

--- include/core/entity.h
+++ include/core/entity.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <raylib.h>
+#include <stdlib.h>
 
 #include "engine.h"
+#include "collision_box2D.h"
 
-typedef struct {
+typedef struct Entity2D {
+  char name[32];
   Vector2 pos;
+  Vector2 size;
   Vector2 velocity;
   float speed;
-  Rectangle collision_box;
-} Entity;
+  CollisionBox2D collision_box;
+  bool is_colliding;
+} Entity2D;
 
-void entity_movement(Entity *entity);
+
+Entity2D entity_create();
+void entity_draw(Entity2D *entity);
+void entity_movement(Entity2D *entity);
--- src/core/entity.c
+++ src/core/entity.c
@@ -1,10 +1,48 @@
 #include "../../include/core/entity.h"
+#include <raylib.h>
 
-void entity_movement(Entity *entity) {
+Entity2D entity_create() {
+  Entity2D entity = (Entity2D){.name = "Default Entity",
+                               .pos = (Vector2){0, 0},
+                               .size = (Vector2){50, 50},
+                               .speed = 0.f,
+                               .velocity = (Vector2){0, 0},
+                               .collision_box = NULL};
+  CollisionBox2D entity_collider = (CollisionBox2D){
+      .name = "Default Collider", .pos = entity.pos, .size = entity.size};
+  entity.collision_box = entity_collider;
+  return entity;
+}
+
+void entity_draw(Entity2D *entity) {
+  DrawRectangleV(entity->pos, entity->size, RED);
+  if (engine.debug_draw) {
+    if (!entity->is_colliding) {
+      DrawRectangleLinesEx((Rectangle){entity->collision_box.pos.x,
+                                       entity->collision_box.pos.y,
+                                       entity->collision_box.size.x,
+                                       entity->collision_box.size.y},
+                           1, BLUE);
+    } else {
+      DrawRectangleLinesEx((Rectangle){entity->collision_box.pos.x,
+                                       entity->collision_box.pos.y,
+                                       entity->collision_box.size.x,
+                                       entity->collision_box.size.y},
+                           1, YELLOW);
+    }
+  }
+}
+
+void entity_movement(Entity2D *entity) {
   entity->speed = 200.0f;
   if (entity->velocity.x != 0 && entity->velocity.y != 0) {
     entity->speed *= 0.7f;
   }
   entity->pos.x += entity->velocity.x * entity->speed * engine.delta_time;
   entity->pos.y += entity->velocity.y * entity->speed * engine.delta_time;
+
+  entity->collision_box.pos.x +=
+      entity->velocity.x * entity->speed * engine.delta_time;
+  entity->collision_box.pos.y +=
+      entity->velocity.y * entity->speed * engine.delta_time;
 }

53f24db31d3d00901ab687df86a1f29dad69d9b2
Richard <richcrwo@gmail.com>
2026-01-25 19:59:31 +0300

    add tile render
    renderer can render tilemap with window size limit.

--- src/core/renderer.c
+++ src/core/renderer.c
@@ -1,6 +1,10 @@
 #include "../../src/core/renderer.h"
 #include "../../src/scene/2d/scene.h"
+#include "engine.h"
+#include <math.h>
 #include <raylib.h>
+#include <stdint.h>
+#include <stdio.h>
 
 Renderer renderer_create() {
   Renderer renderer = {0};
@@ -13,9 +17,35 @@ void renderer_set_scene(Renderer *renderer, Scene *scene) {
 
 void renderer_current_scene(Renderer *renderer) {
   Scene *curr = renderer->current_scene;
-  // TraceLog(LOG_DEBUG, "Current scene pointer: %p", (void *)curr);
-  // TraceLog(LOG_DEBUG, "Entity count: %d", curr->entity_count);
 
+  TileMap *curr_map = curr->map;
+  int width = curr_map->layers[0].width;
+  int height = curr_map->layers[0].height;
+  int tile_width = curr_map->tile_width;
+  int tile_height = curr_map->tile_height;
+  if (curr->map != NULL) {
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        uint16_t gid = curr_map->layers[0].gids[y * width + x];
+        if (gid == 0) {
+          continue;
+        }
+        uint16_t local_id = gid - 1;
+        int tile_x = local_id % width;
+        int tile_y = local_id / width;
+
+        Rectangle src = {tile_x * tile_width, tile_y * tile_height, tile_width,
+                         tile_height};
+        Rectangle dest = {x * tile_width, y * tile_height, tile_width,
+                          tile_height};
+        if (dest.x + dest.width <= 800 && dest.x >= 0 ||
+            dest.x + dest.height >= -450 && dest.x <= 0) {
+          DrawTexturePro(curr_map->tileset, src, dest, (Vector2){0, 0}, 0,
+                         WHITE);
+        }
+      }
+    }
+  }
   // draw entities
   for (int i = 0; i < curr->entity_count; i++) {
     // TraceLog(LOG_DEBUG, "Entity %d size: %f x %f", i,
@@ -40,7 +70,6 @@ void renderer_current_scene(Renderer *renderer) {
       }
     }
   }
-  if (curr->map != NULL) {
-    DrawTexture(curr->map->tileset, 0, 0, WHITE);
-  }
+  int fps = GetFPS();
+  DrawText(TextFormat("%d", fps), 0, 0, 15, BLACK);
 }

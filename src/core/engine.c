14171d8be553dcff88099c0439a323b1bed6aa56
Richard <richcrwo@gmail.com>
2026-01-28 18:41:29 +0300

    add fullscreentoggle and metric update
    add two func - fullscreen toggle and metric update
    also add bool fullscreen to check window state
    add extern metrics for renderer

--- src/core/engine.c
+++ src/core/engine.c
@@ -3,18 +3,22 @@
 #include "../scene/2d/entity.h"
 #include "../scene/2d/physics/collision_box2d.h"
 #include "../scene/2d/scene.h"
+#include "../ui/profiler.h"
 #include <raylib.h>
+#include <stdio.h>
 
 const int WIDTH = 800;
 const int HEIGHT = 450;
 
 Engine engine = {0};
 Renderer renderer = {0};
+Profiler *metrics = {0};
 
 void engine_init() {
   engine.status = START;
   engine.debug_draw = false;
   engine.window_size = (Vector2){WIDTH, HEIGHT};
+  metrics = profiler_init();
   InitWindow(WIDTH, HEIGHT, "metemengine");
   SetWindowState(FLAG_WINDOW_UNDECORATED);
   SetTargetFPS(1000);
@@ -25,7 +29,9 @@ void engine_run() {
   while (!WindowShouldClose() && engine.status == START) {
     engine.delta_time = GetFrameTime();
     on_update();
-
+#ifdef Debug
+    metric_update();
+#endif
     BeginDrawing();
     ClearBackground(BLACK);
     on_draw();
@@ -34,5 +40,23 @@ void engine_run() {
 }
 
 void on_draw() { renderer_current_scene(&renderer); }
+
+void window_fullscreen() {
+  if (engine.w_mode == FULLSCREEN) {
+    ToggleFullscreen();
+    engine.w_mode = FULLSCREEN;
+  } else if (engine.w_mode != FULLSCREEN) {
+    ToggleBorderlessWindowed();
+    engine.w_mode = BORDERLESS;
+  }
+}
+
+void metric_update() {
+  metrics->last_update_time += engine.delta_time;
+  if (metrics->last_update_time >= metrics->update_interval) {
+    profiler_update_data(metrics);
+    metrics->last_update_time = 0.0f;
+  }
+}
 
 void engine_shutdown() { CloseWindow(); }
--- src/core/engine.h
+++ src/core/engine.h
@@ -3,24 +3,27 @@
 #include <stdlib.h>
 
 typedef enum { START, STOP } EngineStatus;
-typedef enum {FULLSCREEN, WINDOWED} WindowMode;
+typedef enum {FULLSCREEN, BORDERLESS} WindowMode;
 
 typedef struct Scene Scene;
 typedef struct Entity2D Entity2D;
 typedef struct CollisionBox2D CollisionBox2D;
 typedef struct Scene Scene;
 typedef struct Renderer Renderer;
-
+typedef struct Profiler Profiler;
 typedef struct {
   float delta_time;
   EngineStatus status;
   bool debug_draw;
   Vector2 window_size;
   WindowMode w_mode;
+  bool fullscreen;
 } Engine;
 
 extern Engine engine;
 extern Renderer renderer;
+extern Profiler *metrics;
+
 void engine_init();
 void engine_run();
 void engine_update();
@@ -29,3 +32,7 @@ void engine_shutdown();
 void on_update();
 void on_draw();
 void draw_debug();
+
+void window_fullscreen();
+
+void metric_update();

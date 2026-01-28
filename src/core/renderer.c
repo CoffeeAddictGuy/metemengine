43b191646834dcba8443657abc409f24b9927797
Richard <richcrwo@gmail.com>
2026-01-28 13:03:43 +0300

    renderer: multi-layer render

--- src/core/renderer.c
+++ src/core/renderer.c
@@ -33,60 +33,60 @@ void renderer_current_scene(Renderer *renderer) {
   int tile_width = curr_map->tile_width;
   int tile_height = curr_map->tile_height;
   if (curr->map != NULL) {
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        uint16_t gid = curr_map->layers[0].gids[y * width + x];
-        if (gid == 0) {
-          continue;
-        }
-        uint16_t local_id = gid - 1;
-        int tile_x = local_id % width;
-        int tile_y = local_id / width;
+    for (int layer = 0; layer < curr_map->layer_count; layer++) {
+      for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+          uint16_t gid = curr_map->layers[layer].gids[y * width + x];
+          if (gid == 0) {
+            continue;
+          }
+          uint16_t local_id = gid - 1;
+          int tile_x = local_id % width;
+          int tile_y = local_id / width;
 
-        Rectangle src = {tile_x * tile_width, tile_y * tile_height, tile_width,
-                         tile_height};
-        Rectangle dest = {x * tile_width, y * tile_height, tile_width,
-                          tile_height};
-        if (has_camera) {
-          Rectangle viewport = {
-              cmr->camera.target.x - engine.window_size.x * 0.5f,
-              cmr->camera.target.y - engine.window_size.y * 0.5f,
-              engine.window_size.x, engine.window_size.y};
-          if (CheckCollisionRecs(viewport, dest)) {
-            DrawTexturePro(curr_map->tileset, src, dest, (Vector2){0, 0}, 0,
-                           WHITE);
-          }
-        } else {
-          if (dest.x + dest.width <= 800 && dest.x >= 0 ||
-              dest.x + dest.height >= -450 && dest.x <= 0) {
-            DrawTexturePro(curr_map->tileset, src, dest, (Vector2){0, 0}, 0,
-                           WHITE);
+          Rectangle src = {tile_x * tile_width, tile_y * tile_height,
+                           tile_width, tile_height};
+          Rectangle dest = {x * tile_width, y * tile_height, tile_width,
+                            tile_height};
+          if (has_camera) {
+            Rectangle viewport = {
+                cmr->camera.target.x - engine.window_size.x * 0.5f,
+                cmr->camera.target.y - engine.window_size.y * 0.5f,
+                engine.window_size.x, engine.window_size.y};
+            if (CheckCollisionRecs(viewport, dest)) {
+              DrawTexturePro(curr_map->tileset, src, dest, (Vector2){0, 0}, 0,
+                             WHITE);
+              // draw entities
+              for (int i = 0; i < curr->entity_count; i++) {
+                DrawRectangleV(curr->entities[i].pos, curr->entities[i].size,
+                               RED);
+                if (engine.debug_draw) {
+                  if (!curr->entities[i].is_colliding) {
+                    DrawRectangleLinesEx(
+                        (Rectangle){curr->entities[i].collision_box.pos.x,
+                                    curr->entities[i].collision_box.pos.y,
+                                    curr->entities[i].collision_box.size.x,
+                                    curr->entities[i].collision_box.size.y},
+                        1, BLUE);
+                  } else {
+                    DrawRectangleLinesEx(
+                        (Rectangle){curr->entities[i].collision_box.pos.x,
+                                    curr->entities[i].collision_box.pos.y,
+                                    curr->entities[i].collision_box.size.x,
+                                    curr->entities[i].collision_box.size.y},
+                        1, YELLOW);
+                  }
+                }
+              }
+            }
+          } else {
+            if (dest.x + dest.width <= 800 && dest.x >= 0 ||
+                dest.x + dest.height >= -450 && dest.x <= 0) {
+              DrawTexturePro(curr_map->tileset, src, dest, (Vector2){0, 0}, 0,
+                             WHITE);
+            }
           }
         }
-      }
-    }
-  }
-  // draw entities
-  for (int i = 0; i < curr->entity_count; i++) {
-    // TraceLog(LOG_DEBUG, "Entity %d size: %f x %f", i,
-    // curr->entities[i].size.x,
-    //          curr->entities[i].size.y);
-    DrawRectangleV(curr->entities[i].pos, curr->entities[i].size, RED);
-    if (engine.debug_draw) {
-      if (!curr->entities[i].is_colliding) {
-        DrawRectangleLinesEx(
-            (Rectangle){curr->entities[i].collision_box.pos.x,
-                        curr->entities[i].collision_box.pos.y,
-                        curr->entities[i].collision_box.size.x,
-                        curr->entities[i].collision_box.size.y},
-            1, BLUE);
-      } else {
-        DrawRectangleLinesEx(
-            (Rectangle){curr->entities[i].collision_box.pos.x,
-                        curr->entities[i].collision_box.pos.y,
-                        curr->entities[i].collision_box.size.x,
-                        curr->entities[i].collision_box.size.y},
-            1, YELLOW);
       }
     }
   }

dae61cbce8d25736fbfc82ff7d8dd0219ba558c1
Richard <richcrwo@gmail.com>
2026-01-28 18:39:55 +0300

    add profiler
    add profiler init, get data (only max ram) and display info
    WIP get all data for linux and windows + add another panel to buttons
    and other things

--- src/ui/profiler.c
+++ src/ui/profiler.c
@@ -1,5 +1,51 @@
 #include "profiler.h"
+#include "../../src/core/engine.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define RAYGUI_IMPLEMENTATION
 #include "../../includes/raygui.h"
 
-int profiler_init() { return GuiPanel((Rectangle){85, 70, 250, 100}, "Hello"); }
+const char ram_info[13] = "/proc/meminfo";
+const char cpu_stat[10] = "/proc/stat";
+
+Profiler *profiler_init() {
+  Profiler *profiler = malloc(sizeof(Profiler));
+  profiler->cpu = 0.0f;
+  profiler->fps = 0;
+  profiler->memory = 0;
+  profiler->pid = getpid();
+  profiler->last_update_time = 0.0f;
+  profiler->update_interval = 5.0f;
+  profiler_update_data(profiler);
+
+  return profiler;
+}
+
+void profiler_print_info(Profiler *profiler) {
+  printf("Pid %d\n", profiler->pid);
+}
+
+void profiler_update_data(Profiler *profiler) {
+// Linux
+#ifdef __linux__
+  FILE *f = fopen(ram_info, "r");
+  char string[128];
+  while (fgets(string, 128, f)) {
+    if (strncmp(string, "MemTotal:", 8) == 0) {
+      printf("MemTotal: %s\n", string);
+    }
+  }
+  fclose(f);
+#elif WIN32
+#endif
+}
+
+void profiler_data_view(Profiler *profiler) {
+  GuiPanel((Rectangle){engine.window_size.x - 300, 0, 300, 200}, "Debug info");
+  if (GuiCheckBox((Rectangle){engine.window_size.x - 280, 50, 10, 10},
+                  "Fullscreen?", &engine.fullscreen)) {
+    window_fullscreen();
+  }
+}
--- src/ui/profiler.h
+++ src/ui/profiler.h
@@ -1,9 +1,29 @@
 #pragma once
 
+#ifdef WIN32
+#include <pdh.h>
+
+#elif __linux__
+#include <sys/sysinfo.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#endif
+
+
 typedef struct Profiler {
+  pid_t pid;
   int fps;
-  int result;
-} Profile;
+  int memory;
+  float cpu;
+  float last_update_time;
+  float update_interval;
+} Profiler;
 
-int profiler_init();
+Profiler *profiler_init();
 
+void profiler_print_info(Profiler *profiler);
+void profiler_update_data(Profiler *profiler);
+void profiler_data_view(Profiler *profiler);

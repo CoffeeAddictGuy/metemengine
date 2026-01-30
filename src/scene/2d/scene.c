077a6a5da5d172d5696d348625b8c9835c3b801d
Richard <richcrwo@gmail.com>
2026-01-30 19:21:46 +0300

    add func scene_add_entity_Pro
    scene_add_entity_Pro used to add entity with custom pos and size

--- src/scene/2d/scene.c
+++ src/scene/2d/scene.c
@@ -1,4 +1,7 @@
 #include "scene.h"
+#include "entity.h"
+#include "tile_map.h"
+#include <raylib.h>
 #include <stdio.h>
 
 Scene scene_create() {
@@ -11,9 +14,17 @@ Scene scene_create() {
 Entity2D *scene_add_entity(Scene *scene) {
   Entity2D entity = entity_create();
   scene->entities[scene->entity_count] = entity;
-  scene->collision_boxes[scene->collision_count] = entity.collision_box;
+  // scene->collision_boxes[scene->collision_count] = entity.collision_box;
   scene->entity_count++;
-  scene->collision_count++;
+  // scene->collision_count++;
+  return &scene->entities[scene->entity_count - 1];
+}
+
+Entity2D *scene_add_entityPro(Scene *scene, Vector2 size, Vector2 pos,
+                              Vector2 atlas_cord) {
+  Entity2D entity = entity_create_Pro(size, pos, atlas_cord);
+  scene->entities[scene->entity_count] = entity;
+  scene->entity_count++;
   return &scene->entities[scene->entity_count - 1];
 }
 
@@ -27,6 +38,11 @@ MCamera2D *scene_add_camera(Scene *scene) {
   }
   printf("Camera name %s!\n", scene->main_camera->name);
   return &scene->cameras[scene->camera_count - 1];
+}
+
+void scene_add_tilemap(Scene *scene, const char *json_path,
+                       const char *tileset_path) {
+  scene->map = tile_map_create(scene, json_path, tileset_path);
 }
 
 void set_main_camera(Scene *scene, MCamera2D *camera) {

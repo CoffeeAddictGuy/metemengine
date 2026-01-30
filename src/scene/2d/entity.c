e1a8d8204e12a85e49797a92220bb579b5c062ef
Richard <richcrwo@gmail.com>
2026-01-30 19:22:12 +0300

    add entity_create_Pro
    same as in scene

--- src/scene/2d/entity.c
+++ src/scene/2d/entity.c
@@ -5,15 +5,25 @@ Entity2D entity_create() {
   Entity2D entity = (Entity2D){.name = "Default Entity",
                                .pos = (Vector2){0, 0},
                                .size = (Vector2){50, 50},
-                               .speed = 0.f,
+                               .atlas_cord = (Vector2){0, 0},
                                .velocity = (Vector2){0, 0},
                                .collision_box = NULL};
-  CollisionBox2D entity_collider = (CollisionBox2D){
-      .name = "Default Collider", .pos = entity.pos, .size = entity.size};
-  entity.collision_box = entity_collider;
+  // CollisionBox2D entity_collider = (CollisionBox2D){
+  //     .name = "Default Collider", .pos = entity.pos, .size = entity.size};
+  // entity.collision_box = entity_collider;
   // TraceLog(LOG_DEBUG, "Created entity collision_box size: %f x %f",
   //          entity.collision_box.size.x, entity.collision_box.size.y);
 
+  return entity;
+}
+
+Entity2D entity_create_Pro(Vector2 size, Vector2 pos, Vector2 atlas_cord) {
+  Entity2D entity = (Entity2D){.name = "Default Entity",
+                               .pos = pos,
+                               .size = size,
+                               .atlas_cord = atlas_cord,
+                               .velocity = (Vector2){0, 0},
+                               .collision_box = NULL};
   return entity;
 }
 

30a084793bdcebf0451ab1e8f06658f3562830d5
Richard <richcrwo@gmail.com>
2026-02-04 18:05:04 +0300

    add engine type and character
    add engine type to auto run all data from scene
    add character2d struct

--- src/scene/2d/entity.h
+++ src/scene/2d/entity.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../../core/engine.h"
+#include "../../core/engine_types.h"
 #include "physics/collision_box2d.h"
 #include <raylib.h>
 
@@ -12,12 +13,38 @@ typedef struct Entity2D {
   Vector2 atlas_cord;
   CollisionBox2D collision_box;
   bool is_colliding;
-
-  void (*entity_on_init) (void);
-  void (*entity_on_update) (void); 
+  
+  void *self;
+  OnInitFn on_init;
+  OnUpdateFn on_update;
+  OnDestroyFn on_destroy;
 } Entity2D;
 
+typedef struct Character2D {
+  char name[32];
+  Vector2 pos;
+  Vector2 size;
+  Vector2 velocity;
+  float speed;
 
-Entity2D entity_create();
+  OnInitFn on_init;
+  OnUpdateFn on_update;
+  OnDestroyFn on_destroy;
+} Character2D;
+
+
+Entity2D entity_create(  
+  void *self,
+  OnInitFn on_init,
+  OnUpdateFn on_update,
+  OnDestroyFn on_destroy
+);
+
+Character2D character2d_create(  
+  OnInitFn on_init,
+  OnUpdateFn on_update,
+  OnDestroyFn on_destroy
+);
+
 Entity2D entity_create_Pro(Vector2 size, Vector2 pos, Vector2 atlas_cord);
-void entity_movement(Entity2D *entity);
+void entity_movement(Character2D *character);
